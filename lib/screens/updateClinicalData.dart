58fb494bc1eb0bbd79a2966134052bf4e0f66451
Anjali <anjalimagar666@gmail.com>
2024-03-11 13:06:14 -0400

    Update Clinical Data of the Patient

---
+++ lib/screens/updateClinicalData.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+
+class Patient {
+  String patientID = '';
+  String date = '';
+  String bloodPressure = '';
+  String respiratoryRate = '';
+  String bloodOxygenLevel = '';
+  String heartbeatRate = '';
+}
+
+class UpdateClinicalData extends StatefulWidget {
+  const UpdateClinicalData({Key? key}) : super(key: key);
+
+  @override
+  State<UpdateClinicalData> createState() => _NewPatientDataState();
+}
+
+class _NewPatientDataState extends State<UpdateClinicalData> {
+  final _formKey = GlobalKey<FormState>();
+  final Patient _patient = Patient();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Update Clinical Record'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Patient ID'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter the patient id!';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _patient.patientID = value!;
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Date'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter the date!';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _patient.date = value!;
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Blood Pressure'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter blood pressure of the patient!';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _patient.bloodPressure = value!;
+                },
+              ),
+              TextFormField(
+                decoration:
+                    const InputDecoration(labelText: 'Respiratory Rate'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter respiratory rate of the patient!';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _patient.respiratoryRate = value!;
+                },
+              ),
+              TextFormField(
+                decoration:
+                    const InputDecoration(labelText: 'Blood Oxygen Level'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter blood oxygen level of the patient!';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _patient.bloodOxygenLevel = value!;
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Heartbeat Rate'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter  heartbeat rate of the patient!';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _patient.heartbeatRate = value!;
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      _formKey.currentState!.save();
+                    }
+                  },
+                  child: const Text('Update'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
